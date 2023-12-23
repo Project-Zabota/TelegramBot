ed8e945a54b54f41c81713cee308ee7102c03bf4
VladaAlmazova <almazova.2004@mail.ru>
2023-12-23 18:07:23 +0500

    отправка новых сообщений пользователя, добавление вопросов и отзывов

--- main.py
+++ main.py
@@ -10,30 +10,46 @@ bot = telebot.TeleBot("6490766765:AAEo1jTAbJeQT3ikeJY1AXGUIu2orT93Nqg", parse_mo
 
 print('....')
 
-
+# TODO удалить
+'''
 user_post = ''
 type_rec = ''
 sub_topic = ''
 request_description = ""
-
+'''
 ChatData = {}
 
 TYPE_PERSON_DEFAULT = 'client'
 TYPE_REQUEST_DEFAULT = 'problem'
 SUBTYPE_REQUEST_DEFAULT = 'els'
+ALLNAMEBUTTON_PROBLEMS = [
+                (('с обслуживанием', 'service'), ['client']),
+                (('с приложением', 'app'), ['client', 'back_office', 'shop']),
+                (('с сайтом', 'website'), ['client', 'back_office', 'shop']),
+                (('с оплатой', 'payment'), ['client', 'shop']),
+                (('с доставкой', 'delivery'), ['client']),
+                (('с сервером', 'server'), ['back_office']),
+                (('с оборудованием', 'equipment'), ['back_office', 'shop']),
+                (('с кассой', 'box_office'), ['shop']),
+                (('другое', 'els'), ['client', 'back_office', 'shop'])
+            ]
 
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    global ChatData
     chat_id = message.chat.id
     ChatData[chat_id] = {'userData': {'name': message.from_user.first_name,
                                       'type_person': TYPE_PERSON_DEFAULT},
+
                          'requestData': {'type_request': TYPE_REQUEST_DEFAULT,
                                          'subtype_request': SUBTYPE_REQUEST_DEFAULT,
                                          'request_text': None}}
-    print(ChatData)
+    q1_definitionPerson(message)
 
+
+def q1_definitionPerson(message):
     markup = types.InlineKeyboardMarkup()
     markup.add(createButton('клиент', 'client'), createButton('сотрудник "Жизньмарт"', 'employee'))
     bot.send_message(message.chat.id, f'Здравствуйте, укажите кто вы', parse_mode='HTML', reply_markup=markup)
@@ -44,114 +60,128 @@ def createButton(title, progName):
     return button
 
 
-
-@bot.callback_query_handler(func=lambda call: True)  # переход к типу запроса
-def callback(call):
-    chat_id = call.message.chat.id
-    global user_post, type_rec, sub_topic
-
-    if call.data in ['employee', 'client', 'shop', 'back_office']:
-        ChatData[chat_id]['userData']['type_person'] = call.data
-        if call.data == "employee":  # call.data это callback_data, которую мы указали при объявлении кнопки
-            markup = types.InlineKeyboardMarkup()
-            markup.add(createButton('магазин', 'shop'), createButton('бэк офис', 'back_office'))
-            bot.send_message(call.message.chat.id, f'Уточните место работы', parse_mode='HTML', reply_markup=markup)
-        elif call.data == "shop":
-            mesg = bot.send_message(call.message.chat.id, 'В каком магазине вы работаете?')
-            bot.register_next_step_handler(mesg, get_text)  # запрос на текст (переходим к блоку кода ниже)
-        else:
-            define_type(call.message.chat.id)
-
-
-    elif call.data == "problem":
-        allNameButton_problems = [
-            (('с обслуживанием', 'service'), ['client']),
-            (('с приложением', 'app'), ['client', 'back_office', 'shop']),
-            (('с сайтом', 'website'), ['client', 'back_office', 'shop']),
-            (('с оплатой', 'payment'), ['client', 'shop']),
-            (('с доставкой', 'delivery'), ['client']),
-            (('с сервером', 'server'), ['back_office']),
-            (('с оборудованием', 'equipment'), ['back_office', 'shop']),
-            (('с кассой', 'box_office'), ['shop']),
-            (('другое', 'els'), ['client', 'back_office', 'shop'])
-        ]
-        person = ChatData[chat_id]['userData']['type_person']
-        markup = createButtonsForType(allNameButton_problems, person)
-        print(person)
-        user_post = person
-        type_rec = "problem"
-        ChatData[chat_id]['requestData']['type_request'] = call.data
-        bot.send_message(call.message.chat.id, f'С чем возникла проблема?', parse_mode='HTML', reply_markup=markup)
+def person_identif_type(chat_id, type_person):
+    global ChatData
+    ChatData[chat_id]['userData']['type_person'] = type_person
+    if type_person == "employee":
+        markup = types.InlineKeyboardMarkup()
+        markup.add(createButton('магазин', 'shop'), createButton('бэк офис', 'back_office'))
+        bot.send_message(chat_id, f'Уточните место работы', parse_mode='HTML', reply_markup=markup)
+    elif type_person == "shop":
+        mesg = bot.send_message(chat_id, 'В каком магазине вы работаете?')
+        bot.register_next_step_handler(mesg, get_text_shop)  # TODO разделить
     else:
-        print(call.data) # проблема с...
-        ChatData[chat_id]['requestData']['subtype_request'] = call.data
-        sub_topic = call.data
-        mesg = bot.send_message(call.message.chat.id, f'Опишите, что случилось', parse_mode='HTML')
-        bot.register_next_step_handler(mesg, get_text_art) # get_text
-        # bot.send_message(call.message.chat.id, f'Ваш запрос принят в работу', parse_mode='HTML')
-
-
-def createButtonsForType(allNameButton, person):
-    markup = types.InlineKeyboardMarkup()
-    for button in allNameButton:
-        if person in button[1]:
-            markup.add(types.InlineKeyboardButton(text=button[0][0], callback_data=button[0][1]))
-    return markup
-
-
-@bot.message_handler(content_types=['text'])  # если соощение это текст, то выполняем
-def get_text(message):
-    request = message.text  # номер или адрес магазина
-    print(request)
-    ChatData[message.chat.id]['requestData']['request_text'] = message.text
-    define_type(message.chat.id)
-
-
-def get_text_art(message):
-    global request_description
-    request = message.text  # номер или адрес магазина
-    print(request)
-    bot.send_message(message.chat.id, f'Ваш запрос принят в работу', parse_mode='HTML')
-    request_description = request
-    send_to_server(message.chat.id)
+        define_type(chat_id)
 
 
 def define_type(chat_id):
     markup = types.InlineKeyboardMarkup()
-    button_problem = types.InlineKeyboardButton(text='есть проблема', callback_data='problem')
-    button_question = types.InlineKeyboardButton(text='задать вопрос', callback_data='question')
-    button_reviews = types.InlineKeyboardButton(text='оставить отзыв', callback_data='reviews')
-    # markup.add(button_question, button_problem, button_reviews)
-    markup.add(button_question)
-    markup.add(button_problem)
-    markup.add(button_reviews)
+    markup.add(createButton('есть проблема', 'problem'))
+    markup.add(createButton('задать вопрос', 'question'))
+    markup.add(createButton('оставить отзыв', 'reviews'))
     bot.send_message(chat_id, f'Чем могу помочь?', parse_mode='HTML', reply_markup=markup)
 
 
+def request_identif_type(chat_id, type_request):
+    global ChatData
+    ChatData[chat_id]['requestData']['type_request'] = type_request
+    if type_request == "problem":
+        person = ChatData[chat_id]['userData']['type_person']
+        markup = createButtonsForType(ALLNAMEBUTTON_PROBLEMS, person)  # создает кнопки по типу пользователя
+        bot.send_message(chat_id, f'С чем возникла проблема?', parse_mode='HTML', reply_markup=markup)
+    elif type_request == "question":
+        mesg = bot.send_message(chat_id, f'Напишите ваш вопрос', parse_mode='HTML')
+        bot.register_next_step_handler(mesg, get_text_art) #TODO разделить
+    elif type_request == "reviews":
+        mesg = bot.send_message(chat_id, f'Пожалуйста, оставте ваш отзыв ниже)', parse_mode='HTML')
+        bot.register_next_step_handler(mesg, get_text_art)  # TODO разделить
+
+
+def createButtonsForType(allNameButton, person):
+    markup_problems_type = types.InlineKeyboardMarkup()
+    for button in allNameButton:
+        if person in button[1]:
+            markup_problems_type.add(createButton(button[0][0], button[0][1]))
+    return markup_problems_type
+
+
+def problem_identif_type(chat_id, type_request):
+    global ChatData
+    ChatData[chat_id]['requestData']['subtype_request'] = type_request
+    mesg = bot.send_message(chat_id, f'Опишите, что случилось', parse_mode='HTML')
+    bot.register_next_step_handler(mesg, get_text_art)  # TODO разделить
+
+
+@bot.callback_query_handler(func=lambda call: True)  # в ответ на нажатие кнопок
+def callback(call):
+    chat_id = call.message.chat.id
+
+    if call.data in ['employee', 'client', 'shop', 'back_office']:
+        person_identif_type(chat_id, call.data)
+
+    elif call.data in ['problem', 'question', 'reviews']:  # кнопки типа запроса
+        request_identif_type(chat_id, call.data)
+
+    elif call.data in [problem[0][1] for problem in ALLNAMEBUTTON_PROBLEMS]:
+        problem_identif_type(chat_id, call.data)
+
+
+@bot.message_handler(content_types=['text'])  # если соощение это текст, то выполняем
+def get_text(message): # новое сообщение от пользователя
+    global ChatData
+    request = message.text
+    print(request)
+    ChatData[message.chat.id]['requestData']['request_text'] = request
+    send_to_server(message.chat.id)
+
+
+def get_text_shop(message):
+    global ChatData
+    request = message.text  # номер или адрес магазина
+    print(request)
+    #TODO пока номер магазина никуда не сохраняется
+    #ChatData[message.chat.id]['requestData']['request_text'] = message.text
+    define_type(message.chat.id)
+
+
+def get_text_art(message):  # запрос пользователя
+    global ChatData
+    request = message.text
+    print(request)
+    ChatData[message.chat.id]['requestData']['request_text'] = request
+    if ChatData[message.chat.id]['requestData']['type_request'] == 'reviews':
+        bot.send_message(message.chat.id, f'Спасибо за вашу обратную связь, вы помогаете нам становиться лучше)', parse_mode='HTML')
+    else:
+        bot.send_message(message.chat.id, f'Ваш запрос принят в работу', parse_mode='HTML')
+    print(ChatData)
+    send_to_server(message.chat.id)
+
+
 def send_to_server(chat_id):
-    data = {
-        # "id": 5,#4,
-        "id": chat_id,
-        "name": ChatData[chat_id]['requestData']['subtype_request'],
-        "user": {
-            "name": ChatData[chat_id]['userData']['name'],
-            "phone": chat_id
-        },
-        "messages": {
-            "sender": "CLIENT",
-            "name": chat_id,
-            "text": ChatData[chat_id]['requestData']['request_text'],
-            "date": datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
-        }
-    }
-    del ChatData[chat_id] #удаляем данные после отправки
+    global ChatData
+    # data = {
+    #     # "id": 5,#4,
+    #     "id": chat_id,
+    #     "name": ChatData[chat_id]['requestData']['subtype_request'],
+    #     "user": {
+    #         "name": ChatData[chat_id]['userData']['name'],
+    #         "phone": chat_id
+    #     },
+    #     "messages": {
+    #         "sender": "CLIENT",
+    #         "name": chat_id,
+    #         "text": ChatData[chat_id]['requestData']['request_text'],
+    #         "date": datetime.datetime.now().strftime("%d.%m.%Y %H:%M:%S")
+    #     }
+    # }
+    # del ChatData[chat_id] #удаляем данные после отправки
 
     createTicket = {
-        "name": sub_topic,
+        "name": ChatData[chat_id]['requestData']['subtype_request'],
         "status": "status",
-        "type": "Problem",
-        "sender": "Client",
-        "priority": 1
+        "type":  ChatData[chat_id]['requestData']['type_request'],#"Problem",
+        "sender": ChatData[chat_id]['userData']['name'], #"Client",
+        "priority": 1 #TODO автоматизировать (1 - высокий 0 - низкий)
     }
 
     # data_json = json.dumps(data)
@@ -159,8 +189,9 @@ def send_to_server(chat_id):
     r = requests.post("http://localhost:5179/api/ticket", json=createTicket) # создается тикет
     addMessage = {
         "sender": "Client",
-        "text": request_description,
-        "timestamp": "05.12.23",
+        #"text": request_description,
+        "text": ChatData[chat_id]['requestData']['request_text'],
+        "timestamp": datetime.datetime.now().strftime("%d.%m.%Y"),
         "ticketId": r.text
     }
     user_by_ticket[int(r.text)] = chat_id
@@ -196,21 +227,28 @@ def send_to_server(chat_id):
 # server_port = 8080
 
 
-user_by_ticket = {}
+user_by_ticket = {} # пишу свой словарь
 server = Flask(__name__)
 
 
 @server.route("/update/", methods=['POST'])
-def processUpdate(): # localhost:5000
+def processUpdate(): #  TODO dict localhost:5000
     body = json.loads(request.json)
+    print(body)
     action = body['action']
     ticket = body['ticket']
     data = body['data']
     if action == "NEW_MESSAGE":
         bot.send_message(user_by_ticket[ticket], data['text'], parse_mode='HTML')
-
     return ""
 
+# {
+#   "action": "NEW_MESSAGE",
+#   "ticket": 1,
+#   "data": {
+#     "text": "нереальный текст"
+#   }
+# }
 
 def bot_polling():
     bot.infinity_polling()
